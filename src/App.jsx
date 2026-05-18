9c99ddec3355be5a96bb6eb10b3e18c7c655f0e4
Sam Khan <samadkhan040204@gmail.com>
2026-05-18 16:31:49 +0530

    setuped

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
